982ef0a2f020578424a5bd0baa8642489438580d
edwar4rd <edwar4rd@gmail.com>
2025-05-16 17:08:56 +0800

    feat: try getting audio device configs before starting ratatui

---
+++ src/audio.rs
@@ -0,0 +1,55 @@
+use color_eyre::{
+    Result,
+    eyre::{WrapErr, eyre},
+};
+use cpal::traits::{DeviceTrait, HostTrait};
+
+pub fn host_device_setup() -> Result<(
+    cpal::Host,
+    cpal::SupportedStreamConfig,
+    cpal::Device,
+    cpal::Device,
+)> {
+    let host = cpal::host_from_id(cpal::HostId::Jack).unwrap_or_else(|_| {
+        println!("Jack/Pipewire host not available, falling back to default host");
+        cpal::default_host()
+    });
+
+    let in_device = host
+        .default_input_device()
+        .ok_or_else(|| eyre!("Default input device is not available"))?;
+    println!("Input device : {}", in_device.name()?);
+
+    let in_config = in_device.default_input_config()?;
+    println!("Input config : {:?}", in_config);
+
+    let out_device = host
+        .default_output_device()
+        .ok_or_else(|| eyre!("Default output device is not available"))?;
+    println!("Output device : {}", out_device.name()?);
+
+    let out_config = out_device
+        .supported_output_configs()?
+        .find(|config| {
+            config.channels() == in_config.channels()
+                && config.sample_format() == in_config.sample_format()
+                && config
+                    .try_with_sample_rate(in_config.sample_rate())
+                    .is_some()
+        })
+        .map(|range| range.with_sample_rate(in_config.sample_rate()))
+        .ok_or_else(|| eyre!("Output device doesn't support the same format as the input"))?;
+
+    assert_eq!(in_config, out_config);
+
+    println!("Output config : {:?}", out_config);
+
+    Ok((host, in_config, in_device, out_device))
+}
+
+#[test]
+fn test_host_device_setup() {
+    let result = host_device_setup();
+    assert!(result.is_ok());
+    let _ = result.unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use color_eyre::Result;
 use crossterm::event::{self, Event};
+use loooper::audio;
 use ratatui::{DefaultTerminal, Frame};
+
+pub struct State {
+    bpm: f32,
+    time_signature: (u32, u32),
+    metronome: bool,
+    measure: u32,
+    sink: cpal::Stream,
+    source: cpal::Stream,
+}
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+    let _ = audio::host_device_setup()?;
     let terminal = ratatui::init();
     let result = run(terminal);
     ratatui::restore();
