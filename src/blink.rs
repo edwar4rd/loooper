d42a6df3a3a015cd40a87472771aba963fd4a7a9
edwar4rd <edwar4rd@gmail.com>
2025-06-09 21:23:52 +0800

    feat: sync blink with beat

--- src/blink.rs
+++ src/blink.rs
@@ -1,21 +1,24 @@
 use tokio::time;
 use wiringpi::pin::Value;
 
-pub async fn blink(_current_millibeat: std::sync::Arc<std::sync::atomic::AtomicU32>) {
+pub async fn blink(current_millibeat: std::sync::Arc<std::sync::atomic::AtomicU32>) {
     let pi = wiringpi::setup();
     let pin_1 = pi.output_pin(15);
-    let pin_2 = pi.output_pin(16);
+    let _pin_2 = pi.output_pin(16);
 
-    let interval = time::Duration::from_millis(500);
+    let interval = time::Duration::from_millis(1);
+    let mut current_state = Value::Low;
 
     loop {
-        pin_1.digital_write(Value::High);
-        time::sleep(interval).await;
-        pin_2.digital_write(Value::High);
-        time::sleep(interval).await;
-        pin_2.digital_write(Value::Low);
-        time::sleep(interval).await;
-        pin_1.digital_write(Value::Low);
+        if current_millibeat.load(std::sync::atomic::Ordering::Relaxed) % 1000 < 250 {
+            if current_state == Value::Low {
+                current_state = Value::High;
+                pin_1.digital_write(Value::High);
+            }
+        } else if current_state == Value::High {
+            current_state = Value::Low;
+            pin_1.digital_write(Value::Low);
+        }
         time::sleep(interval).await;
     }
 }
