e685d8cd7735c8d07eff01371a671251c6e1ff27
edwar4rd <edwar4rd@gmail.com>
2025-06-13 01:29:57 +0800

    refactor: prepare to add zero crossing function to drum

--- src/audio/sample.rs
+++ src/audio/sample.rs
@@ -1,3 +1,4 @@
+use std::num::NonZeroU32;
 use std::path::Path;
 use std::sync::Arc;
 
@@ -6,9 +7,19 @@ pub struct SamplePad {
     pub buffer: Arc<[f32]>,
     pub pos: usize,
     pub playing: bool,
+    pub reset_scheduled: Option<NonZeroU32>,
 }
 
 impl SamplePad {
+    pub fn empty() -> Self {
+        SamplePad {
+            buffer: Arc::new([]),
+            pos: 0,
+            playing: false,
+            reset_scheduled: None,
+        }
+    }
+
     pub fn load_from_wav(path: &Path) -> color_eyre::Result<Self> {
         let mut reader = hound::WavReader::open(path)?;
         let buffer = reader
@@ -19,7 +30,16 @@ impl SamplePad {
             buffer,
             pos: 0,
             playing: false,
+            reset_scheduled: None,
         })
+    }
+
+    pub fn start(&mut self) {
+        if !self.playing {
+            self.pos = 0;
+            self.playing = true;
+            self.reset_scheduled = None;
+        }
     }
 
     pub fn next_sample(&mut self) -> f32 {
