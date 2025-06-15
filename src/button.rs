ef5528ffef2e181ee2cde1320ac55a6ebd0d9071
edwar4rd <edwar4rd@gmail.com>
2025-06-15 18:23:00 +0800

    refactor: use rppal's poll_interrupts for button input

--- src/button.rs
+++ src/button.rs
@@ -1,6 +1,5 @@
 use color_eyre::Result;
-use rppal::gpio::Level;
-use std::{thread::sleep, time::Duration};
+use std::time::Duration;
 
 const BUTTON_PINS: [u8; 13] = [23, 22, 21, 3, 2, 0, 7, 27, 26, 15, 16, 5, 6];
 
@@ -11,37 +10,35 @@ pub fn button(
 ) -> Result<()> {
     let gpio = rppal::gpio::Gpio::new()?;
     let mut pins = Vec::new();
-    let mut last_states = vec![false; BUTTON_PINS.len()];
-    for pin in BUTTON_PINS {
-        let pin = gpio.get(pin)?.into_input_pullup();
+    for (button_id, pin) in BUTTON_PINS.iter().enumerate() {
+        let mut pin = gpio.get(*pin)?.into_input_pullup();
+        if (1..=8).contains(&button_id) {
+            pin.set_interrupt(rppal::gpio::Trigger::Both, Some(Duration::from_millis(50)))?;
+        } else {
+            pin.set_interrupt(
+                rppal::gpio::Trigger::FallingEdge,
+                Some(Duration::from_millis(50)),
+            )?;
+        }
         pins.push(pin);
     }
+    let polled_pins: Vec<&rppal::gpio::InputPin> = pins.iter().collect();
 
     let interval = Duration::from_micros(100);
 
     loop {
-        for ((button_id, pin), last_state) in
-            pins.iter_mut().enumerate().zip(last_states.iter_mut())
-        {
-            let pin_state = pin.read();
-            if pin_state == Level::Low && !(*last_state) {
-                *last_state = true;
-                if button_id > 8 {
-                    let pad_id = button_id - 9;
-                    let _ = pad_tx.send(pad_id);
-                }
-                let _ = button_tx.send(button_id);
-            } else if pin_state == Level::High {
-                // Button released
-                *last_state = false;
+        if let Some((pin, _event)) = gpio.poll_interrupts(&polled_pins, false, Some(interval))? {
+            let button_id = BUTTON_PINS.iter().position(|&p| p == pin.pin()).unwrap() + 1;
+            if button_id > 8 {
+                let pad_id = button_id - 9;
+                let _ = pad_tx.send(pad_id);
             }
+            let _ = button_tx.send(button_id);
         }
 
         if shutdown.try_recv().is_ok() {
             break;
         }
-
-        sleep(interval);
     }
 
     Ok(())
